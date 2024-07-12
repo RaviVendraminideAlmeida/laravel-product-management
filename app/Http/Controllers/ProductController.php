362f661c4505e02d87723e080795682c02f1c449
Ravi_Almeida <ravivendramini@gmail.com>
2024-07-12 16:31:55 -0300

    feat: created product controller, model and migration

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+
+class ProductController extends Controller
+{
+    public function update(Request $request){
+    }
+    public function store(Request $request){
+    }
+    public function find(Request $request){
+
+    }
+    public function show(Request $request){
+
+    }
+}
+
+
+
