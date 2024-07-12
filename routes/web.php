6612fe5a93479db15b0ff192d43af5910e6b4fbd
Ravi_Almeida <ravivendramini@gmail.com>
2024-07-12 16:31:09 -0300

    refactor: removed welcome page route and view

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return response()->json([ "Hello" => "World"]);
 });
 
 Route::get('/dashboard', function () {
